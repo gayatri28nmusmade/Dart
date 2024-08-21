bd5ff26035a6e67888317a36234de59672697b79
GayatriMusmade <fluttergayatrii@gmail.com>
2024-08-21 22:49:33 +0530

    String Interpolation

---
+++ string_interpolation/4fourth.dart
@@ -0,0 +1,13 @@
+void main(){
+  int empId=20;
+  print("My employee id is $empId");
+
+  String firstName="Incubators";
+  print("My fist name is $firstName");
+
+  String lastName="System";
+  print("My last name is $lastName");
+
+  //concat
+  print("First Name $firstName Last Name:$lastName");
+}
---
+++ string_interpolation/5fifth.dart
@@ -0,0 +1,4 @@
+void main(){
+  String firstName="Incubators";
+  print("My name is $firstName and length is ${firstName.length}");
+}
